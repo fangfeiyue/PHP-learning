fc550de384d42135c45ae63ca85109ec1229c8ec
FangFeiyue <18766131540@163.com>
2018-03-19 10:19:04 +0800

    可变参数列表

--- demo1/11函数/06.php
+++ demo1/11函数/06.php
@@ -3,7 +3,7 @@
  * @Author: FangFeiyue 
  * @Date: 2018-03-16 13:26:45 
  * @Last Modified by: FangFeiyue
- * @Last Modified time: 2018-03-16 13:44:25
+ * @Last Modified time: 2018-03-16 14:15:28
  * @function: 可变参数列表
  */
 header('content-type:text/html;charset=utf-8');
@@ -17,20 +17,26 @@ function test($a, $b){
 test(1, 2);
 
 // 实现var-dump()函数
-
+echo '<br/>';
 function var_dump2(){
     $len = func_num_args();
-    
+
     if ($len <= 0){
         return;
     }
 
+    $params = func_get_args();
+
     foreach($params as $param){
-        echo $params.'&&'.$param;
+        if (is_numeric($param)){
+            echo 'int('.$param.')';
+        }else if(is_string($param)){
+            echo 'string('.strlen($param).')'.$param;
+        }
     }
 }
 
-var_dump2(1, 2, 3, 4);
+var_dump2(1, 2, 3, 4, 'fangfeiyue');
 
 
 
