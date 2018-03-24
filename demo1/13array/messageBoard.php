d8bd73a890406bc50f0742094bc40366ec839c6c
FangFeiyue <18766131540@163.com>
2018-03-24 18:09:52 +0800

    留言板页面简单布局

---
+++ demo1/13array/messageBoard.php
@@ -0,0 +1,102 @@
+<html>
+
+<head>
+	<meta charset="UTF-8">
+	<script type="text/javascript" src="http://www.francescomalagrino.com/BootstrapPageGenerator/3/js/jquery-2.0.0.min.js"></script>
+	<script type="text/javascript" src="http://www.francescomalagrino.com/BootstrapPageGenerator/3/js/jquery-ui"></script>
+	<link href="http://www.francescomalagrino.com/BootstrapPageGenerator/3/css/bootstrap-combined.min.css" rel="stylesheet" media="screen">
+	<script type="text/javascript" src="http://www.francescomalagrino.com/BootstrapPageGenerator/3/js/bootstrap.min.js"></script>
+</head>
+
+<body>
+	<div class="container-fluid">
+		<div class="row-fluid">
+			<div class="span12">
+				<div class="page-header">
+					<h1>
+						<small><span>留言板1.0</span></small>
+					</h1>
+				</div>
+				<div class="hero-unit">
+					<h1>
+						Hello, world!
+					</h1>
+					<p>
+						这是一个可视化布局模板, 你可以点击模板里的文字进行修改, 也可以通过点击弹出的编辑框进行富文本修改. 拖动区块能实现排序.
+					</p>
+					<p>
+						<a rel="nofollow" class="btn btn-primary btn-large" href="#">参看更多 »</a>
+					</p>
+				</div>
+				<table class="table">
+					<thead>
+						<tr>
+							<th>
+								编号
+							</th>
+							<th>
+								用户名
+							</th>
+							<th>
+								标题
+							</th>
+							<th>
+								时间
+							</th>
+							<th>
+								内容
+							</th>
+						</tr>
+					</thead>
+					<tbody>
+						<tr class="success">
+							<td>
+								1
+							</td>
+							<td>
+								TB - Monthly
+							</td>
+							<td>
+								01/04/2012
+							</td>
+							<td>
+								Default
+							</td>
+							<td>
+								Default
+							</td>
+						</tr>
+					</tbody>
+				</table>
+				<form class="form-horizontal" action="#" method="post">
+					<div>请留言</div>
+					<div class="control-group">
+						<label class="control-label" for="inputEmail">用户名</label>
+						<div class="controls">
+							<input id="inputEmail" name="userName" type="text" />
+						</div>
+					</div>
+					<div class="control-group">
+						<label class="control-label" for="inputPassword">标题</label>
+						<div class="controls">
+							<input id="inputPassword" type="text" name="title" />
+						</div>
+					</div>
+					<div class="control-group">
+						<label class="control-label" for="inputPassword">内容</label>
+						<div class="controls">
+							<textarea name="content" rows="5" cols="30"></textarea>
+						</div>
+					</div>
+					<div class="control-group">
+						<div class="controls">
+							<input type="submit" class="btn btn-primary" value="发布留言"/>
+						</div>
+					</div>
+				</form>
+			</div>
+		</div>
+	</div>
+</body>
+
+</html>
