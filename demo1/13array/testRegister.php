e5ab315ba1006849306b3d57c162da31b649936f
FangFeiyue <18766131540@163.com>
2018-03-24 17:41:42 +0800

    拼接字符串

--- demo1/13array/testRegister.php
+++ demo1/13array/testRegister.php
@@ -8,3 +8,18 @@ $email = $_POST['email'];
 $userName = $_POST['userName'];
 
 print_r(compact('userName', 'pwd', 'email'));
+
+$arr = compact('userName', 'pwd', 'email');
+
+$keys = array_keys($arr);
+$values = array_values($arr);
+
+// insert (x, x, x) values ('x', 'x', 'x');
+
+$keysStr = join(',', $keys);
+echo $keysStr;
+echo '<hr/>';
+$valueStr = "'".join("','", $values)."'";
+echo $valueStr;
+echo '<hr/>';
+echo "insert (${keysStr}) values (${valueStr});";
