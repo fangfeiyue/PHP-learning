ee319f8c0e154c95d44f3860003c146c424b3463
FangFeiyue <18766131540@163.com>
2018-03-27 10:08:18 +0800

    fwirte

--- demo1/15file/04.php
+++ demo1/15file/04.php
@@ -5,9 +5,16 @@ header('content-type:text/html;charset=utf-8');
 
 $fileName = __DIR__.'/01.txt';
 
-$handle = fopen($fileName, 'rd+');
+$handle = fopen($fileName, 'w');
 
-if (fwrite($handle, 'fangfeiyue')){
+// if (fwrite($handle, 'fangfeiyue')){
+//     echo '写入成功';
+// }else{
+//     echo '写入失败';
+// }
+
+
+if (fputs($handle, '123456')){
     echo '写入成功';
 }else{
     echo '写入失败';
