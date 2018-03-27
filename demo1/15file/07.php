c9d054f97cc27e087f7f3c039ab1fdc60cb0a7ad
FangFeiyue <18766131540@163.com>
2018-03-27 13:30:12 +0800

    csv格式操作

---
+++ demo1/15file/07.php
@@ -0,0 +1,23 @@
+<?php
+date_default_timezone_set('prc');
+header('content-type:text/html;charset=utf-8');
+
+
+$fileName = 'user3.csv';
+$handle   = fopen($fileName, 'wb+');
+// $data     = [
+//     [1, 'php', 'php是最好的语言'],
+//     [2, 'js', 'js是最火的语言']
+// ];
+
+$data = [
+    ['id'=>'1', 'userName'=>'fang', 'age'=>'3'],
+    ['id'=>'2', 'userName'=>'fei', 'age'=>'4'],
+];
+
+foreach ($data as $val){
+    fputcsv($handle, $val);
+}
+
+fclose($handle);
+
