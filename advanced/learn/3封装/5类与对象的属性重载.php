070ed71571ad9e8e480e634321942402dfa54a45
FangFeiyue <18766131540@163.com>
2018-06-15 18:35:46 +0800

    属性重载练习

--- advanced/learn/3封装/5类与对象的属性重载.php
+++ advanced/learn/3封装/5类与对象的属性重载.php
@@ -27,3 +27,48 @@ $xiao = New MyClass;
 
 isset($xiao->age);
 unset($xiao->age);
+
+
+
+
+echo '<br/>';
+class Example{
+    //不可访问的属性
+    protected $name = 'fang';
+    //不可访问的普通方法    
+	protected function getName () {
+        echo $this->name;
+    }
+    //不可访问的静态方法
+    private static function test () {
+        
+    }
+	//魔术方法
+    public function __set ($n, $v) {
+        echo '给不可访问的属性赋值时我被触发:__set<br/>';
+    }
+    public function __get ($n) {
+        echo '调用不可以访问的属性时我被触发:__get<br/>';
+    }
+    public function __isset ($n) {
+        echo '对不可访问的属性使用empty()或者isset()函数时我被触发:__isset<br/>';
+    }
+    public function __unset ($n) {
+        echo '对不可访问的属性使用unset函数时，我被触发：__unset<br/>';
+    }
+    public function __call ($n, $v) {
+        echo '调用不可访问的普通方法时我们触发:__call<br/>';
+    }
+    public static function __callStatic ($n, $v) {
+        echo '调用不可访问的静态方法时我被触发:__callStatic<br/>';
+    }
+}
+//实例化对象
+$exa = new Example;
+//触发各种魔术方法
+$exa->name;
+$exa->name = 'yue';
+isset($exa->name);
+unset($exa->name);
+$exa->getName();
+$exa->test();
