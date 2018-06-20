95c0d6cd9dafbf5594e6f72a43d78e1ae9c9b1c8
FangFeiyue <18766131540@163.com>
2018-06-20 13:40:48 +0800

    PHP多文件上传结构搭建

--- advanced/learn/fileupload/multipleUpload/controller/upload.class.php
+++ advanced/learn/fileupload/multipleUpload/controller/upload.class.php
@@ -1,4 +1,13 @@
 <?php
-header('content-type:text/html;charset=utf-8');
 
-if (!defined(APPROT)) exit;
+if (!defined('APPROOT')) exit;
+
+class Upload {
+    private $file = array();    // 上传的多个文件信息
+    private $return;    // 上传结果
+    
+    // 显示上传模板
+    public function showUploadForm ($templateName) {
+        echo file_get_contents(APPROOT.'/view/'.$templateName.'.html');
+    }
+}
--- advanced/learn/fileupload/multipleUpload/index.php
+++ advanced/learn/fileupload/multipleUpload/index.php
@@ -1,5 +1,18 @@
 <?php
-header('content-type:text/html;charset=utf-8');
 
-// 定义根目录常量
+//定义根目录常量
 define('APPROOT', str_replace('\\', '/', dirname(__FILE__)));
+//实例化Upload上传对象
+require_once APPROOT.'/controller/Upload.class.php';
+$upload = new Upload();
+//初始化$action参数
+$action = empty($_GET['action']) || !in_array($_GET['action'], array('show', 'upload'))
+	? 'show' : $_GET['action'];
+
+if($action == 'show') {
+	$type = empty($_GET['type']) || !in_array($_GET['type'], array('singleName', 'groupName'))
+		? 'singleName' : $_GET['type'];
+	$upload->showUploadForm($type);
+} elseif($action == 'upload') {
+	$upload->handleUploadFile();
+}
